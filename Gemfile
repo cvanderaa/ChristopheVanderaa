a9ddc14605fa94b0dda22efdb366ede0b523b8b6
cvanderaa <vanderaa.christophe@gmail.com>
2025-03-02 11:15:58 +0100

    Use minimal_mistakes as a remote theme

--- Gemfile
+++ Gemfile
@@ -2,7 +2,8 @@ source "https://rubygems.org"
 
 gem "github-pages", group: :jekyll_plugins
 
-gem "jekyll","=3.9.2"
+gem "tzinfo-data"
+gem "wdm", "~> 0.1.0" if Gem.win_platform?
 
 group :jekyll_plugins do
   gem "jekyll-paginate"
@@ -10,6 +11,5 @@ group :jekyll_plugins do
   gem "jekyll-gist"
   gem "jekyll-feed"
   gem "jemoji"
+  gem "jekyll-include-cache"
 end
-
-gem "webrick", "~> 1.8"
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -4,4 +4,19 @@ var store = [{
         "tags": ["research","conference"],
         "url": "/2024/05/03/attending-conferences.html",
         "teaser": "/assets/images/avatar.jpg"
-      }]
+      },{
+    "title": "About me",
+    "excerpt":"I am a postdoctoral researcher at Ghent University, working under the supervision of Prof. Lieven Clement in the statOmics group. My research currently focuses on the analysis of mass spectrometry-based single-cell proteomics. I have a background in biomedical sciences and in bioinformatics, which I combined during a PhD in Prof....","url": "http://localhost:4000/about/"
+  },{
+    "title": "Blog",
+    "excerpt":"conference Attending conferences 2024-05-03 2 minute read In this post, I list all the conferences I have attended and, for some, the material I have presented. research Attending conferences 2024-05-03 2 minute read In this post, I list all the conferences I have attended and, for some, the material I...","url": "http://localhost:4000/blog/"
+  },{
+    "title": "Hi, I'm Chris",
+    "excerpt":"                                                                                                                     Research                                                    Get a snapshot of my scientific output.                                                   Learn more                                                                                                                                                    Blog                                                    Personal thoughts, experiments and projects.                                                   Learn more                                        ","url": "http://localhost:4000/"
+  },{
+    "title": "My research",
+    "excerpt":"(Updated on October 2024) My research mainly focuses on the development of tools and practices to analyze mass spectrometry-based single-cell proteomics (SCP) data. I achieved several milestones during my PhD at Prof. Laurent Gatto’s lab. I established the first standardized data structure for mass spectrometry-based single-cell proteomics with the development...","url": "http://localhost:4000/research/"
+  },{
+    "title": "Posts by Tags",
+    "excerpt":"conference Attending conferences 2024-05-03 2 minute read In this post, I list all the conferences I have attended and, for some, the material I have presented. research Attending conferences 2024-05-03 2 minute read In this post, I list all the conferences I have attended and, for some, the material I...","url": "http://localhost:4000/tags/"
+  }]
